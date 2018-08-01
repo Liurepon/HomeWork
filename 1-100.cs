eed3ee48c8f82b762254e888a9a6013a41e6ffd2
劉睿鵬 <41939521+Liurepon@users.noreply.github.com>
2018-08-01 15:00:01 +0800

    1+到100

---
+++ 1-100.cs
@@ -0,0 +1,24 @@
+using System;
+class Program {
+    static void Main(string[] args) {
+       
+        Console.WriteLine("????????????????????????????????");
+        
+        int i = 1;
+        int sum = 0 ;
+        
+
+        while( i <=100)
+        {
+            sum = sum + i;
+            i++;
+        }
+        Console.WriteLine(sum);
+
+
+        
+        Console.WriteLine("################################");
+        Console.WriteLine(); 
+        Console.WriteLine();
+    }
+}
